20d0e5ea513497a2ceb5642539407fb11c40cc80
donghon85 <doghon85@naver.com>
2022-11-17 14:26:45 +0900

    836. Rectangle Overlap.py : the problem is solved.

---
+++ Matrix/836. Rectangle Overlap.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        # overlab w * h
+        overlab_area = max(min(rec1[2], rec2[2]) - max(rec1[0], rec2[0]), 0) * max(min(rec1[3], rec2[3]) - max(rec1[1], rec2[1]), 0)
+        return False if overlab_area == 0 else True
