c9a3cac681809142164ffe00fd950fa7d1ea005c
donghon85 <doghon85@naver.com>
2022-11-22 15:20:08 +0900

    279. Perfect Squares.py : the problem is solved(add comment)

--- DP/279. Perfect Squares.py
+++ DP/279. Perfect Squares.py
@@ -3,18 +3,18 @@ class Solution:
         square = [inf for _ in range(n+1)]
 
         for i in range(n+1):
-            if i*i < n+1:
+            if i*i < n+1: # create square[n*n] = 1
                 square[i*i] = 1
 
         for i in range(n+1):
-            if square[i] > 1:
+            if square[i] > 1: # skip the square[n*n]
                 m = inf
                 for j in range(1, n+1):
-                    if j*j > i:
+                    if j*j > i: # out of range
                         break
                     if square[i-j*j] < m:
                         m = square[i-j*j]
-                    if m == 1:
+                    if m == 1: # if 'i-j*j' is n*n+1, then break
                         break
                 square[i] = m+1
         return square[n]
