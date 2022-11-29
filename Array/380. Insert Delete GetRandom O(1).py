2fb06840c997ae6856a14f769ba41578a050c84e
donghon85 <doghon85@naver.com>
2022-11-29 14:33:40 +0900

    380. Insert Delete GetRandom O(1).py : the problem is solved.

---
+++ Array/380. Insert Delete GetRandom O(1).py
@@ -0,0 +1,25 @@
+class RandomizedSet:
+    import random
+    def __init__(self):
+        self.container = set()
+
+    def insert(self, val: int) -> bool:
+        if val not in self.container:
+            self.container.add(val)
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        if val in self.container:
+            self.container.remove(val)
+            return True
+        return False
+    def getRandom(self) -> int:
+        return random.choice(list(self.container))
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
