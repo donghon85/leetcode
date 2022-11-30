6c7fd8e87b87aba25ebf4988997abe07406feb10
donghon85 <doghon85@naver.com>
2022-11-30 14:07:50 +0900

    1207. Unique Number of Occurrences.py : the problem is solved.

---
+++ Array/1207. Unique Number of Occurrences.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        c = Counter(arr)
+        return True if len(c.values()) == len(set(c.values())) else False
